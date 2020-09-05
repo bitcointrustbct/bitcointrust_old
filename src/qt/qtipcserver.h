252161c8c4a6a19701dcfe6846cdbbe598d17db2
BitcoinTrust BCT <info@bitcointrust.global>
2020-09-05 09:10:41 -0500

    Add files via upload

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define BitcoinTrust-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "BitcoinTrustURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
