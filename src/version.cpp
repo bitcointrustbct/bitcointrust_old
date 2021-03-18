32471504664e5474ff191bfc874a442b0bea6b35
BitcoinTrust BCT <info@bitcointrust.global>
2021-03-18 14:16:58 -0500

    Update version.cpp
    Version 1.1.1.2 Update

--- src/version.cpp
+++ src/version.cpp
@@ -11,7 +11,7 @@
 const std::string CLIENT_NAME("BitcoinTrust");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   "1.0.1.1"
+#define CLIENT_VERSION_SUFFIX   "1.1.1.2"
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
